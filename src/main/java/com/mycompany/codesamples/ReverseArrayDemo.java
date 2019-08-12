acc73256508332e976d623b97a5ab35c8fea63b8
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-08-11 19:59:53 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/codesamples/ReverseArrayDemo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.codesamples;
+
+import java.util.Arrays;
+/**
+ *
+ * @author baur
+ */
+public class ReverseArrayDemo {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(ReverseArray.reverse(array)));
+    }
+}
