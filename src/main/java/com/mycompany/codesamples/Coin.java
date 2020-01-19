5bcbed53a8d01231a6c8b8fca3ac17c851977bd3
Baur Urazalinov <burazalinov@gmail.com>
2020-01-19 12:32:12 -0600

    Added Coin class. Added CoinDemo class.

---
+++ src/main/java/com/mycompany/codesamples/Coin.java
@@ -0,0 +1,34 @@
+package com.mycompany.codesamples;
+
+import java.util.Random;
+
+/**
+ *
+ * @author baur
+ */
+public class Coin {
+
+    public final static int HEADS = 0;
+    public final static int TAILS = 1;
+
+    private int faceUp;
+
+    public Coin() {
+        flip();
+    }
+
+    public void flip() {
+        Random rand = new Random();
+        faceUp = rand.nextInt(2);
+    }
+
+    public String getFaceUp() {
+        if (faceUp == HEADS) {
+            return "Heads";
+        } else if (faceUp == TAILS) {
+            return "Tails";
+        } else {
+            return "Invalid";
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/codesamples/CoinDemo.java
@@ -0,0 +1,16 @@
+package com.mycompany.codesamples;
+
+/**
+ *
+ * @author baur
+ */
+public class CoinDemo {
+    public static void main(String[] args) {
+        Coin c = new Coin();
+        System.out.println("Initial: " + c.getFaceUp());
+        for(int i = 0; i < 10; i++) {
+            c.flip();
+            System.out.println("After Flip: " + c.getFaceUp());
+        }
+    }
+}
